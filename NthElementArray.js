c62fe58fa7f2ce415b78359aa1e956a05d88ba02
Bita <122091308+Bita04@users.noreply.github.com>
2023-11-17 02:12:04 +0330

    Simple Projects N4

---
+++ NthElementArray.js
@@ -0,0 +1,8 @@
+//Write a JavaScript program to get the nth element of a given array of elements.
+a=prompt("Enter your Array:");
+b=prompt("witch element do you want:");
+element=(a,b)=>{
+
+    console.log(a[b])
+}
+element(a,b);
