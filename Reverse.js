292a5133bb9fcbda6b308b8223f656d027a5d68d
Bita <122091308+Bita04@users.noreply.github.com>
2023-11-17 02:11:32 +0330

    Simple Projects N3

---
+++ Reverse.js
@@ -0,0 +1,25 @@
+//Write a JavaScript program to reverse the order of characters in the string.
+const a=prompt("ENter your string:")
+reverse = (a)=>{
+    console.log("hiiiiiiiiiii")
+    const b=a.split("");
+    for (let i=a.length-1 ; i==0 ;i--){
+        console.log(b[i])
+    }
+    
+}
+reverse(a);
+
+// const a=prompt("ENter your string:")
+// reverse = (a)=>{
+//     console.log("hiiiiiiiiiii")
+    
+//     const b=a.split("");
+//     console.log(b)  
+//   for (let i=a.length;i==0 ;i--){
+//         console.log("BYEEEEE")
+//         console.log(b[i])
+//     }
+    
+// }
+// reverse(a);
