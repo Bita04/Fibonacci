63f6f2c62c6a2c28b15a8a3aeb339db9b5e3934d
Bita <122091308+Bita04@users.noreply.github.com>
2023-11-17 02:12:39 +0330

    Simple Projects N5

---
+++ lowerCase.js
@@ -0,0 +1,9 @@
+//write a JavaScript program to check if a string is lower case or not
+const a =prompt("Enter your word:")
+lowerCase =(a) =>{
+    if(a.toLowerCase()==a){
+        console.log("True")
+    }
+    else console.log("False")
+}
+lowerCase(a);
